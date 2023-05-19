765c24b9e82ca3fb7874f328077f6cef7b44adb4
Luisa Agudelo <luisaab1103@gmail.com>
2023-05-19 05:44:06 -0500

    Se realiza operación para sumar dígitos de un numero

---
+++ 02-Ejercicios/Suma_digitos.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Suma_digitos{
+    public static void main(String []args){
+        Scanner sumad = new Scanner(System.in);
+
+        int suma, num, cifra;
+        
+
+        System.out.println("Ingrese el número");
+        num = sumad.nextInt();
+        suma = 0;
+
+        while (num != 0){
+
+        cifra = num % 10;
+        suma = suma + cifra;
+        num = num /10;
+
+        System.out.println("La suma de los digitos es : "+suma);
+
+        }
+
+
+    }
+
+
+}
