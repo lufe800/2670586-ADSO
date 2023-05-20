ea02e195fb448c7422350ddcee30d8d3fe976b58
Luisa Agudelo <luisaab1103@gmail.com>
2023-05-19 22:05:06 -0500

    Se realiza ejercicio con condicional para hallar número par o impar

--- 03-Condiciones/Ejercicio_02.java
+++ 03-Condiciones/Ejercicio_02.java
@@ -2,16 +2,22 @@ import java.util.Scanner;
 
 public class Ejercicio_02{
     public static void main(String []args){
-        Scanner palabras = new Scanner(System.in);
+        Scanner entrada = new Scanner(System.in);
 
         int numero;
 
-        System.out.println("Ingresa un número de tres digitos");
+        System.out.print("Ingresa un número de tres dígitos: ");
+        numero = entrada.nextInt();
 
-        if (numero >= 100)
-        System.out.println("");
-
-
+        if (numero>=100 && numero <=999){
+            if(numero%2 == 0){
+                System.out.println("El número "+ numero +" es par");
+            }else{
+                System.out.println("El número "+ numero + " es impar");
+            }
+        }else{
+            System.out.println("El número ingresado no es valido");
+        }
 
     }
     
