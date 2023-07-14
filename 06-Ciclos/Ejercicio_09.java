0052de290d37a22286ef2869f0c804dc90fb0b80
Luisa Agudelo <luisaab1103@gmail.com>
2023-07-13 23:27:29 -0500

    Se realiza ejercicio 9 donde se imprime raíz digital

--- 06-Ciclos/Ejercicio_09.java
+++ 06-Ciclos/Ejercicio_09.java
@@ -9,22 +9,39 @@ public class Ejercicio_09{
         int digito = 0;
         int sumador = 0;
         int numero = n;
-        
+        int sumaTotal = 0;
+        int sumaDos = 0;
+                 
+        String numeroCadena = String.valueOf(n); 
+        String numerosString = "", stringTotal= "";
         System.out.println();
         System.out.println("Raíz dígital: ");
         while(n != 0){
             digito = n % 10;
             n = n / 10;
-            sumador = sumador + digito;
-
+            sumador = sumador + digito;            
             if(n == 0 && sumador > 9){
                 n = sumador;
-                sumador = 0;    
+                sumador = 0;                    
             }   
-            
+
         } 
-        System.out.print(digito+" + "+digito+ " = "+sumador);  
+        for(int i = 0; i < numeroCadena.length(); i++) {
+            int digits = Character.getNumericValue(numeroCadena.charAt(i));
+            sumaTotal += digits;
+            stringTotal = String.valueOf(sumaTotal);
+            numerosString += String.valueOf(digits) +" + "; 
+        }
+
+        int digito_1 = Character.getNumericValue(numeroCadena.charAt(0));
+        int digito_2 = Character.getNumericValue(numeroCadena.charAt(numeroCadena.length()-1));
+        sumaDos = digito_1 + digito_2;
+        String res = String.valueOf(digito_1) +" + " + String.valueOf(digito_2) +" = "+ String.valueOf(sumaDos);
+
+        System.out.println(numerosString + " = "+ stringTotal);
+        System.out.println(res);
         System.out.print("\nLa raíz dígital de " +numero+" es: "+sumador);
+        
         
 
     }
