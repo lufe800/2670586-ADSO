4b4cdacec7815b3aec638a75a9e699b684d3a3fa
Luisa Agudelo <luisaab1103@gmail.com>
2023-08-11 10:52:01 -0500

    Se realiza ejercicio con array donde se almacenan 2 arreglos en un tercero y se ordenan los elementos en orden ascendente

---
+++ 09-Arrays/Ejercicio_11.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+public class Ejercicio_11{
+    public static void main(String[] args){
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print(" => Ingrese un número entero N: ");
+		int numeroN = entrada.nextInt();
+
+        System.out.print(" => Ingrese un número entero M: ");
+		int numeroM = entrada.nextInt();
+
+        int arregloA[] = new int[numeroN];
+        System.out.print("\nArreglo 1 : ");
+        for(int i = 0; i < arregloA.length; i++){
+            arregloA[i] = (int)(Math.random()* numeroN);
+            System.out.print("{ "+arregloA[i]+ " }");     
+        }
+
+        int arregloB[] = new int[numeroM];
+        System.out.print("\nArreglo 2 : ");
+        for(int i = 0; i < arregloB.length;i++){
+            arregloB[i] = (int)(Math.random()*numeroM);
+            System.out.print("{ "+arregloB[i]+ " }"); 
+        }    
+
+        int arregloC[] = new int[arregloA.length + arregloB.length]; // sumo las dos longitudes de los arreglos a y b en el arreglo c
+        for(int i = 0; i < arregloA.length; i++){// copiar el arreglo a al arreglo c
+            arregloC[i] = arregloA[i];
+        }
+        for(int i = 0; i < arregloB.length; i++){
+            arregloC[arregloA.length + i] = arregloB[i]; // copio el arreglo b al c con longitud a
+        }
+        
+        //ordenar de forma ascendente
+        System.out.print("\nArreglo 3 : ");
+        
+        for(int i = 0; i < arregloC.length -1; i++){//recorre el arreglo, hasta poner en posición final al elemento mas grande
+            for(int j = 0; j < arregloC.length -1-i; j++){// segunda pasada para eliminar posición de indice ya asignada
+                if(arregloC[j] > arregloC[j + 1]){
+                    int aux = arregloC[j];
+                    arregloC[j] = arregloC[j + 1];
+                    arregloC[j + 1] = aux;
+                }
+
+            }
+        }
+        for (int i = 0; i < arregloC.length -1; i++) {
+            System.out.print("{ "+arregloC[i+1]+ " }"); 
+        } 
+        
+       
+        
+        
+
+    }
+}
