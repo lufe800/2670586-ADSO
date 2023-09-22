64d90b0ad3f3ece946ac859bc238923c7fd69107
Luisa Agudelo <luisaab1103@gmail.com>
2023-09-22 06:01:25 -0500

    Se crea función para hallar numero primo retornando un valor boleano

---
+++ 14-Funcion/Ejercicio_12.java
@@ -0,0 +1,57 @@
+public class Ejercicio_12{
+
+    public static void main(String[]args){
+        System.out.println("+----------------------+");
+        System.out.println("+    FUNCIÓN PRIMO     +");
+        System.out.println("+----------------------+");
+
+        int numero = 17;
+        boolean numPrimo = numPrimo(numero);
+
+        if(numPrimo){
+            System.out.println("=> "+numero + " Es primo.");
+        }else{
+            System.out.println("=> "+numero + " No es primo.");
+        }
+
+        System.out.println("");
+
+        numero = 1;
+        numPrimo = numPrimo(numero);
+
+        if(numPrimo){
+            System.out.println("=> "+numero + " Es primo.");
+        }else{
+            System.out.println("=> "+numero + " No es primo.");
+        }
+
+        System.out.println("");
+
+        numero = 4;
+        numPrimo = numPrimo(numero);
+
+        if(numPrimo){
+            System.out.println("=> "+numero + " Es primo.");
+        }else{
+            System.out.println("=> "+numero + " No es primo.");
+        }
+
+
+
+
+    }
+
+    public static boolean numPrimo(int numero){
+
+        if(numero <= 1){
+            return false;
+        }
+        for(int i=2; i<numero; i++){
+            if(numero % i == 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
