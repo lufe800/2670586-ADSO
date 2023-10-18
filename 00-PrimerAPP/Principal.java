2c38e1e583a7cc9446baf2db2e3d5920c4704964
Luisa Agudelo <luisaab1103@gmail.com>
2023-10-18 08:10:50 -0500

    Se modifica nombre de carpeta a 00-PrimerAPP

---
+++ 00-PrimerAPP/Principal.java
@@ -0,0 +1,26 @@
+public class Principal{
+    public static void main (String [] args){
+
+        System.out.println( "++------------------------------++");
+        System.out.println( "++--- SISTEMA DE FACTURACION ---++");
+        System.out.println( "++------------------------------++");
+        System.out.println( "||                              ||");
+        System.out.println( "||                Cliente:89    ||");
+        System.out.println( "||                Vendedores:13 ||");
+        System.out.println( "||                Productos:130 ||");
+        System.out.println( "||                Facturas:30   ||");
+        System.out.println( "||                              ||");
+        System.out.println( "||  1.Creacion de cliente       ||");
+        System.out.println( "||  2.Creacion de vendedor      ||");
+        System.out.println( "||  3.Creacion de factura       ||");
+        System.out.println( "||  4.Ver lista de clientes     ||");
+        System.out.println( "||  5.Ver lista de vendedores   ||");
+        System.out.println( "||  6.Ver lista de facturas     ||");
+        System.out.println( "||  7.Ver detalle de factura    ||");
+        System.out.println( "||  8.Salir                     ||");
+        System.out.println( "++------------------------------++");
+        System.out.println( "++------------------------------++");  
+
+    }
+
+}
